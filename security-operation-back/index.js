29eaf8ec32864a639b9ffe60d5d5e90ff890a2ef
d0422 <rlfehd2013@naver.com>
2024-10-08 16:36:43 +0900

    feat: 비밀번호 입력 추가

--- security-operation-back/index.js
+++ security-operation-back/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql2/promise');
 const cors = require('cors');
+const crypto = require('crypto');
+
 require('dotenv').config();
 
 const { PORT, DB_PORT, HOST, DB_USER, PASSWORD, DB } = process.env;
@@ -23,19 +25,31 @@ const appStart = async () => {
   app.get('/', async (req, res) => {
     try {
       const [results] = await connection.query('SELECT * FROM `guestbook`');
+      const result = results.map((element) => ({
+        id: element.id,
+        name: element.name,
+        message: element.message,
+        created_at: element.created_at,
+      }));
 
-      res.send(results);
+      res.send(result);
     } catch (err) {
       console.log(err);
     }
   });
 
   app.post('/', async (req, res) => {
-    const { name, message } = req.body;
+    const { name, message, password } = req.body;
+    const hashPassword = crypto
+      .createHash('sha512')
+      .update(password + PASSWORD) //salt
+      .digest('hex');
+
     await connection.query(
-      'INSERT INTO guestbook (name, message) values (?, ?)',
-      [name, message]
+      'INSERT INTO guestbook (name, message, hash) values (?, ?, ?)',
+      [name, message, hashPassword]
     );
+
     res.json({
       message: 'ok',
     });
@@ -43,6 +57,23 @@ const appStart = async () => {
 
   app.delete('/', async (req, res) => {
     const id = req.query?.id;
+    const password = req.query?.password;
+    const [results] = await connection.query(
+      'SELECT * FROM `guestbook` where id=?',
+      [id]
+    );
+
+    const hashPassword = crypto
+      .createHash('sha512')
+      .update(password + PASSWORD) //salt
+      .digest('hex');
+
+    const isRight = results[0].hash === hashPassword;
+    if (!isRight) {
+      res.status(400).json({
+        message: '비밀번호가 다릅니다.',
+      });
+    }
     await connection.query('DELETE from guestbook where id=?', [id]);
     res.json({
       message: 'ok',
@@ -51,7 +82,20 @@ const appStart = async () => {
 
   app.patch('/', async (req, res) => {
     const id = req.query?.id;
-    const { name, message } = req.body;
+    const { name, message, password } = req.body;
+
+    const hashPassword = crypto
+      .createHash('sha512')
+      .update(password + PASSWORD) //salt
+      .digest('hex');
+
+    const isRight = results[0].hash === hashPassword;
+    if (!isRight) {
+      res.status(400).json({
+        message: '비밀번호가 다릅니다.',
+      });
+    }
+
     await connection.query(
       'UPDATE guestbook SET name=?, message=? where id=?',
       [name, message, id]
